e311a6dc36d608ff9ea7f0b72b7f2cb12ab9c25b
Franz Neumann <noname@x230.lan>
2022-07-17 11:43:36 +0200

    fix path

--- start-smartmeter.sh
+++ start-smartmeter.sh
@@ -6,6 +6,6 @@
 # app=$(dirname $0)/dbus-smartmeter.py
 
 # start -x -s $tty
-app="python3 dbus-smartmeter.py"
+app="python3 /opt/victronenergy/dbus-smartmeter/dbus-smartmeter.py"
 args=" /dev/$tty"
 start $args
